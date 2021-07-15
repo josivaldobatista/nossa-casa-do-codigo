b19beef183485beb24c7cb9145130e3d5a34bcb7
Josivaldo Batista <josivaldo-fb@outlook.com>
2021-07-15 18:25:10 -0300

    Definindo headers e status customizados

--- src/main/kotlin/br/com/zup/autores/AutorController.kt
+++ src/main/kotlin/br/com/zup/autores/AutorController.kt
@@ -1,11 +1,13 @@
 package br.com.zup.autores
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpResponse.created
 import io.micronaut.http.HttpResponse.ok
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.validation.Valid
 
@@ -14,10 +16,14 @@ import javax.validation.Valid
 class AutorController(val autorRepository: AutorRepository) {
 
   @Post
-  fun cadastrar(@Body @Valid request: AutorRequest) {
+  fun cadastrar(@Body @Valid request: AutorRequest): HttpResponse<Any> {
     val autor = request.toModel()
     autorRepository.save(autor)
-    println("Autor: Nome: ${autor.nome}, Email: ${autor.email}, Descrição: ${autor.descricao}")
+
+    val uri = UriBuilder.of("/api/autores/{id}")
+      .expand(mutableMapOf(Pair("id", autor.id)))
+
+    return created(uri)
   }
 
   @Get
